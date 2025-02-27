d8481e5fbcb905cdda1ee574cafac14b406f28f4
JanLascala <lascala.jan@gmail.com>
2025-02-27 15:07:28 +0100

    able to generate random emails

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 console.log("work")
+
+
+fetch(`https://flynn.boolean.careers/exercises/api/random/mail`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+
+    .catch(Error => {
+
+        console.error(error);
+    })
+
+
+
+
+
+fetch("https://flynn.boolean.careers/exercises/api/random/int")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+
+    .catch(Error => {
+
+        console.error(error);
+    })
